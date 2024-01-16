1e85fa889eb8466918f38b6707d1ebba95ba8b2b
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2024-01-16 09:50:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-//your JS code here. If required.
 function throttle(callback, delay) {
+  let timeoutId;
+  let lastArgs;
 
+  const throttled = function(...args) {
+    lastArgs = args;
+    if (!timeoutId) {
+      callback.apply(this, lastArgs);
+      timeoutId = setTimeout(() => {
+        timeoutId = null;
+        if (lastArgs) {
+          callback.apply(this, lastArgs);
+          lastArgs = null;
+        }
+      }, delay);
+    }
+  };
+
+  throttled.cancel = function() {
+    clearTimeout(timeoutId);
+    timeoutId = null;
+    lastArgs = null;
+  };
+
+  return throttled;
 }
-
-module.exports = throttle;
+module.exports= throttle
